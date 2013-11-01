6bcbf8d54a89bdf6c0c99bbfb456c45ae518e7b1
Lars Harzem <lars@harzem.de>
2013-11-01 12:08:50 +0100

    extend with redis hset

--- lib/private_pub/faye_extension.rb
+++ lib/private_pub/faye_extension.rb
@@ -1,10 +1,24 @@
+require 'redis'
+
 module PrivatePub
   # This class is an extension for the Faye::RackAdapter.
   # It is used inside of PrivatePub.faye_app.
   class FayeExtension
+
+		Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
+		
     # Callback to handle incoming Faye messages. This authenticates both
     # subscribe and publish calls.
     def incoming(message, callback)
+			hash_string = Redis.current.hgetall('subscriptions')
+			if hash_string && !hash_string.empty?
+				# subscriptions = eval(hash_string)
+				key = hash_string.find{|k, v| eval(v)[:client_id] == message['clientId']}
+				if key
+					Redis.current.hset('subscriptions', key.first, {time: Time.now.to_i, client_id: eval(hash_string[key.first])[:client_id]})
+				end
+			end
+			# Redis.current.set(channel, client_id)
       if message["channel"] == "/meta/subscribe"
         authenticate_subscribe(message)
       elsif message["channel"] !~ %r{^/meta/}
@@ -22,6 +36,9 @@ module PrivatePub
         message["error"] = "Incorrect signature."
       elsif PrivatePub.signature_expired? message["ext"]["private_pub_timestamp"].to_i
         message["error"] = "Signature has expired."
+			else
+				Redis.current.hset('subscriptions', message["subscription"], {time: Time.now.to_i, client_id: message['clientId']})
+				puts "\nPP redis hset: #{Redis.current.hgetall('subscriptions')}\n"
       end
     end
 
