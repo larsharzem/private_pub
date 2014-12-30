776b2b0de1803b8fedc13adf1d0f91f3e16f01e5
Lars Harzem <lars@harzem.de>
2014-12-30 16:35:51 +0100

    changed redis to allow more than one client id (for users with more then one open tab), added deletion of subscriptions when the user disconnects from faye

--- lib/private_pub/faye_extension.rb
+++ lib/private_pub/faye_extension.rb
@@ -13,26 +13,47 @@ module PrivatePub
 		# Callback to handle incoming Faye messages. This authenticates both
 		# subscribe and publish calls.
 		def incoming(message, callback)
-			## begin try
-			begin
-				hash_string = Redis.current.hgetall('subscriptions')
-				#Redis.current.hset('log', "#{Time.now.to_i}_inco", {called_method: "incoming", message: message, hash_string: hash_string})
-				if hash_string && !hash_string.empty?
-					key = hash_string.find{|k, v| eval(v)[:client_id] == message['clientId']}
-					if key && key.first.index('/feed/actor') == 0
-						Redis.current.hset('subscriptions', key.first, {time: Time.now.to_i, client_id: eval(hash_string[key.first])[:client_id], called_method: "incoming"})
-					end
-				end
-			rescue Exception => e
-				puts "\nException: #{e}\n"
-			end
-			## end try
+			puts "\n#{Time.now} incoming, msg", message
 			
 			if message["channel"] == "/meta/subscribe"
 				authenticate_subscribe(message)
 			elsif message["channel"] !~ %r{^/meta/}
 				authenticate_publish(message)
+			else
+				## begin try
+				begin
+					hash_string = Redis.current.hgetall('subscriptions')
+					#Redis.current.hset('log', "#{Time.now.to_i}_inco", {called_method: "incoming", message: message, hash_string: hash_string})
+					if hash_string && !hash_string.empty?
+						key = hash_string.find{|k, v| eval(v)[:client_ids] != nil && eval(v)[:client_ids].include?(message['clientId'])}
+						#puts "key: #{key}"
+						if key && key.first.index('/feed/actor') == 0
+							channel = key.first
+							clean_hash = eval(hash_string[channel])
+							#puts "clean", clean_hash
+							if message['channel'] == '/meta/disconnect'
+								puts "DISCONNECTED"
+								if (clean_hash[:client_ids].length > 1)
+									puts "deleting one"
+									new_client_ids = clean_hash[:client_ids].delete(message['clientId'])
+									new_client_ids = clean_hash[:client_ids]
+									#puts new_client_ids
+									Redis.current.hset('subscriptions', channel, {time: Time.now.to_i, client_ids: new_client_ids, called_method: "incoming"})
+								else
+									puts "deleting all"
+									Redis.current.hdel('subscriptions', channel)
+								end
+							else
+								Redis.current.hset('subscriptions', channel, {time: Time.now.to_i, client_ids: clean_hash[:client_ids], called_method: "incoming"})
+							end
+						end
+					end
+				rescue Exception => e
+					puts "\nException: #{e}\n"
+				end
+				## end try
 			end
+			
 			message['data']['channel'] ||= message['channel'] if message['data']
 			callback.call(message)
 		end
@@ -48,9 +69,19 @@ module PrivatePub
 				elsif PrivatePub.signature_expired? message["ext"]["private_pub_timestamp"].to_i
 					message["error"] = "Signature has expired."
 				elsif message["subscription"].index('/feed/actor') == 0
+				
+					hash_string = Redis.current.hgetall('subscriptions')
+					present_subscription = hash_string[message["subscription"]]
+					#puts "already present sub: #{present_subscription}"
+					if present_subscription
+						clean_hash = eval(present_subscription)
+						client_ids = clean_hash[:client_ids].push(message['clientId'])
+					else
+						client_ids = [message['clientId']]
+					end
 					## begin try
 					begin
-						Redis.current.hset('subscriptions', message["subscription"], {time: Time.now.to_i, client_id: message['clientId'], called_method: "authenticate_subscribe"})
+						Redis.current.hset('subscriptions', message["subscription"], {time: Time.now.to_i, client_ids: client_ids, called_method: "authenticate_subscribe"})
 					rescue Exception => e
 						puts "\nException: #{e}\n"
 					end
